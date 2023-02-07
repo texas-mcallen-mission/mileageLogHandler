0a64067d50fc56dfdd2e626e5deeb7021a1825f0
harrierpigeon <32232925+HarrierPigeon@users.noreply.github.com>
2023-02-07 16:13:39 -0600

    wrote sort test

---
+++ sortTests.ts
@@ -0,0 +1,34 @@
+function sortTests() {
+    const array1:kiDataEntry[] = [
+        { ki1: 45, ki2: 26, ki3: 31, ki4: "asodidn" },
+        { ki1: 16, ki2: 9, ki3: 4, ki4: "sdfionsdiog" },
+        { ki1: 21, ki2: 37, ki3: 0, ki4: "sodfisndifo" },
+        { ki1: 41, ki2: 25, ki3: 50, ki4: "sdfionsdfioo" },
+        { ki1: 20, ki2: 9, ki3: 1, ki4: "IONDFIOSDN" },
+        { ki1: 17, ki2: 8, ki3: 1, ki4: "IODN" },
+        { ki1: 11, ki2: 27, ki3: 39, ki4: "🤣" },
+        { ki1: 0, ki2: 37, ki3: 9, ki4: "null" },
+        { ki1: 46, ki2: 32, ki3: 26, ki4: "" },
+        { ki1: 19, ki2: 15, ki3: 35, ki4: "ionsdf" },
+        { ki1: 47, ki2: 0, ki3: 36, ki4: "aiosnd" },
+        { ki1: 44, ki2: 28, ki3: 13, ki4: "eiinio" },
+        { ki1: 8, ki2: 23, ki3: 13, ki4: "iodnone" },
+        { ki1: 44, ki2: 34, ki3: 15, ki4: "oisnoDISFN" },
+        { ki1: 0, ki2: 9, ki3: 45, ki4: "aa" },
+        { ki1: 21, ki2: 44, ki3: 28, ki4: "aa" },
+        { ki1: 35, ki2: 31, ki3: 27, ki4: "a a" },
+    ]
+
+    const class1 = new kiDataClass(array1)
+    const outObj1 = class1.sort("ki1").end
+    const outObj2 = class1.sort("ki2").end
+    const outObj3 = class1.sort("ki3").end
+    const outObj4 = class1.sort("ki4").end
+
+    console.log(outObj1)
+    console.log(outObj2)
+    console.log(outObj3)
+    console.log(outObj4)
+
+    console.log("Tests completed!")
+}
