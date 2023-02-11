ffaac1f80b00cc9a8280efbe9f85d9c9c51280c3
HarrierPigeon <32232925+HarrierPigeon@users.noreply.github.com>
2023-02-11 15:57:30 -0600

    Time for a buncha stuff to happen

--- slideSorter.ts
+++ slideSorter.ts
@@ -1,7 +1,36 @@
+
+
+/**
+ * Load data
+ * Assign CRUD iterant
+ * Possible: filter out old stuff
+ * per month:
+ *      get presentation
+ *      filter data
+ *      sort data by GC#
+ *      make index of slides
+ *      move slides around
+ */
+
+
+/**
+ * @description
+ * @param {GoogleAppsScript.Slides.Presentation} presentation
+ * @param {string} objectId
+ * @return {*}  {GoogleAppsScript.Slides.Slide}
+ */
+function getSlideFromObjectID(presentation: GoogleAppsScript.Slides.Presentation, objectId: string): GoogleAppsScript.Slides.Slide {
+    let output = presentation.getSlideById(objectId);
+    return output;
+}
+
+/**
+ * @description monolithic slide sorter
+ */
 function sortSlides() {
-    let sortSheet = new SheetData(new RawSheetData(datastoreConfig))
-    let rawData = sortSheet.getData()
-    let sortData = new kiDataClass(convertKisToSlideEntries(rawData))
+    const sortSheet = new SheetData(new RawSheetData(datastoreConfig))
+    const rawData = sortSheet.getData()
+    const sortData = new kiDataClass(convertKisToSlideEntries(rawData))
 
     // this is a bit more of a pain than I thought it was going to be...
     /*
@@ -12,4 +41,49 @@ function sortSlides() {
        */
 
     // need to go hit the whiteboard for a bit before writing this
+    // whiteboard has been hit
+
+    const splitKeys = [
+        "year",
+        "month"
+    ]
+    
+    const outData = sortData.groupDataByMultipleKeys(splitKeys)
+
+    // since we grouped this by year, then month, this will give us an array of kiDataEntries stored at data[year][month]
+    for (const year in outData) {
+        const yearData = outData[year]
+        for (const month in yearData) {
+            const presentationObj = getLogbook(year, month);
+            // I'm so lazy I'd rather make more classes than rewrite code
+            // maybe that just means I'm predisposed to making libraries???
+
+            const monthData: kiDataEntry[] = yearData[month];
+            const dataClass = new kiDataClass(monthData);
+            // sort from biggest to smallest number
+            const sorterArgs: sortArgs = {
+                descending: true,
+                valueType: sortTypes.number
+            };
+            dataClass.sort("gasCard", sorterArgs);
+
+            // take the sorted data and turn it into slideDataEntries, which guarantee types.
+            const slideData = convertKisToSlideEntries(dataClass.end);
+
+            // make an array of slides
+            const slidePositions: string[] = [];
+            for (const entry of slideData) {
+                // spread operator: basically the same as for looping through an array and pushing all the data separately
+                slidePositions.push(...entry.slideIdArray);
+            }
+            
+            for (let i = 0; i < slidePositions.length;i++) {
+                const slide = getSlideFromObjectID(presentationObj, slidePositions[i])
+                slide.move(i+1)
+            }
+
+        }
+    }
 }
+
+
